b528019a4e442718dc987ee2e4cd7cef3a10b822
Angela <angelarose.de@gmail.com>
2018-09-13 13:50:24 -0500

    Likes And Dislikes

---
+++ src/outputexamples/LikesAndDislikes.java
@@ -0,0 +1,12 @@
+package outputexamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Likes Dislikes");
+		System.out.println("Art   Math");
+		System.out.println("Photography  Kids");
+	}
+
+}
