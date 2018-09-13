6c24456c18510d8cab83e71edf84384bed14f7c2
Angela <angelarose.de@gmail.com>
2018-09-13 13:16:57 -0500

    Full Name and Age

---
+++ src/outputexamples/NameAndAge.java
@@ -0,0 +1,12 @@
+package outputexamples;
+
+public class NameAndAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Angela");
+System.out.println("McCollum");
+System.out.println(17);
+	}
+
+}
