37009f35108228d11bad6a5e8e236bc4d959fcc7
Angela <angelarose.de@gmail.com>
2018-09-13 13:50:33 -0500

    Initials

---
+++ src/outputexamples/Initials.java
@@ -0,0 +1,16 @@
+package outputexamples;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("      A       RRRRR    M       M");
+		System.out.println("     A A      R    R   M M   M M");
+		System.out.println("    A   A     R     R  M   M   M");
+		System.out.println("   AAAAAAAA   RRRRRR   M   M   M");
+		System.out.println("  A       A   R R      M       M");
+		System.out.println(" A         A  R  R     M       M");
+		System.out.println("A           A R   R    M       M");
+	}
+
+}
