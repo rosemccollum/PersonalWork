a50c2b545bbe10cfe77508a2af96ba42d8e30d19
Angela <angelarose.de@gmail.com>
2018-09-13 13:53:42 -0500

    Likes And Dislikes Update

--- src/outputexamples/LikesAndDislikes.java
+++ src/outputexamples/LikesAndDislikes.java
@@ -4,9 +4,12 @@ public class LikesAndDislikes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Likes Dislikes");
-		System.out.println("Art   Math");
-		System.out.println("Photography  Kids");
+		System.out.println("Likes        Dislikes");
+		System.out.println("Art          Math");
+		System.out.println("Photography  Stupid People");
+		System.out.println("Music        Country Music");
+		System.out.println("Jewlery      Yellow");
+		System.out.println("Blue         Humidity");
 	}
 
 }
