4892ecbd8faba79b64eef9df8dc05c7681fc24cf
Angela <angelarose.de@gmail.com>
2018-09-19 14:48:03 -0500

    Mailing Address Update #2

--- src/outputexamples/MailingAddress.java
+++ src/outputexamples/MailingAddress.java
@@ -4,7 +4,7 @@ public class MailingAddress {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("16455 Georgetown Dr Apt 105 \n Brookfield, WI. 53005");
+		System.out.println("16455 Georgetown Dr Apt 105 \nBrookfield, WI. 53005");
 	}
 
 }
