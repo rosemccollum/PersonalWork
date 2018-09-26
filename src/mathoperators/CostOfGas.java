af241c38babcbc60e6bb37acea9fa618359364e2
Angela <angelarose.de@gmail.com>
2018-09-26 13:29:09 -0500

    Gas Ex. Finished

--- src/mathoperators/CostOfGas.java
+++ src/mathoperators/CostOfGas.java
@@ -10,16 +10,18 @@ double money = -999.99;
 double totalgallons = -999.99;
 double averagegas = -999.99;
 Scanner input = new Scanner(System.in);
-System.out.println("How far was the road trip?");
+System.out.println("How many miles was the road trip?");
 trip = input.nextDouble();
 System.out.println("How many miles per gallon does your vehicle get?");
 mpg = input.nextDouble();
 System.out.println("How much money did you spend on gas?");
 money = input.nextDouble();
-
+totalgallons = trip / mpg;
+averagegas = money/totalgallons;
 System.out.println("You traveled " + trip + " miles" );
 System.out.println("You spent $" + money + " on gas" );
-System.out.println("You used " + trip + " gallons of gas" );
+System.out.println("You used " + totalgallons + " gallons of gas" );
+System.out.println("You spent an average of $" + averagegas + " per gallon on gas");
 	}
 
 }
