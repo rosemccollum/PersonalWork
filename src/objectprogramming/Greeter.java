e69f546c278b648f0da79b355a680156af838c1d
Angela <angelarose.de@gmail.com>
2018-09-28 14:56:38 -0500

    Constructors

---
+++ src/objectprogramming/Greeter.java
@@ -0,0 +1,15 @@
+package objectprogramming;
+//import statements at the top
+public class Greeter {
+// instance fields - data storage
+	
+// constructors - methods for constructing the object
+	
+// default constructor
+public Greeter ()
+{
+	//Statements to initialize the state of the object
+}
+//methods - things the object can do
+}
+
