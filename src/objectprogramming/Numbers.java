f7055b85831e5016b9219db24281ff368791a4f0
Angela <angelarose.de@gmail.com>
2018-10-04 13:43:26 -0500

    Numbers Ex.

---
+++ src/objectprogramming/Numbers.java
@@ -0,0 +1,14 @@
+package objectprogramming;
+
+public class Numbers {
+
+public Numbers()
+{
+	
+}
+	public void sayNumber(double num) //double num = local variable declaration. void = no return
+	{
+		System.out.println(num);
+	}
+
+}
--- src/objectprogramming/TestClass.java
+++ src/objectprogramming/TestClass.java
@@ -6,10 +6,8 @@ public  TestClass(){
 }	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Kid bob = new Kid();
-	bob.sayMama();
-	bob.sayFood();
-	bob.sayStink();
+	Numbers counter = new Numbers();
+			counter.sayNumber(7.5);
 
 }
 }
