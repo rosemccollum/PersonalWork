21ac6d6ffdce673a121252a78f0455eae9f74447
Angela <angelarose.de@gmail.com>
2018-10-11 13:22:53 -0500

    Neighbor Ex.

---
+++ src/objectprogramming/Neighbor.java
@@ -0,0 +1,24 @@
+package objectprogramming;
+
+public class Neighbor {
+
+public Neighbor()
+{
+}
+public void sayJoke()
+{
+	System.out.println("Your life");
+}
+public void sayMoney()
+{
+	System.out.println("Gimme my fucking money");
+}
+public void sayConversation()
+{
+	System.out.println("Hey, how are you");
+	System.out.println("I'm dying");
+	System.out.println("Mood");
+	System.out.println("No seriously. I'm dying. I have cancer");
+	System.out.println("Well RIP dude");
+}
+}
--- src/objectprogramming/TestClass.java
+++ src/objectprogramming/TestClass.java
@@ -6,8 +6,9 @@ public  TestClass(){
 }	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Numbers area = new Numbers();
-			double answer = area.returnRoundUp(5.6);
-			System.out.println(answer);
+	Neighbor Jane = new Neighbor();
+			Jane.sayJoke();
+			Jane.sayMoney();
+			Jane.sayConversation();
 }
 }
