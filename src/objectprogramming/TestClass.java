3e7de4117f9eb454bee7c2a366516cb0da6f8a89
Angela <angelarose.de@gmail.com>
2018-10-12 13:41:50 -0500

    Worker Ex.

--- src/objectprogramming/TestClass.java
+++ src/objectprogramming/TestClass.java
@@ -6,17 +6,10 @@ public  TestClass(){
 }	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Sandbox filler = new Sandbox();
+	Worker ken = new Worker();
 	Scanner input = new Scanner(System.in);
-		System.out.println("How long is the sandbox in feet?");
-		int length = input.nextInt();
-		System.out.println("How deep is the sandbox in feet?");
-		int depth = input.nextInt();
-		System.out.println("How wide is the sandbox in feet?");
-		int width = input.nextInt();
-		System.out.println("How many cubic feet of sand can your transport per hour?");
-		int rate = input.nextInt();
-		filler.fillSandbox3(length, depth, width, rate);
-		System.out.println("It will take " + time + " hours to fill the sandbox");
+		System.out.println("Your paycheck is " + ken.payCheck() + " dollars a week");
+		ken.changePay(4);
+		System.out.println("Your new hourly pay is " + ken.getNewpay());
 }
 }
