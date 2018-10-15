32e0da20ee0ee4cf86da754dba21fdab5d83a785
Unknown <angelarose.de@gmail.com>
2018-10-14 21:51:32 -0500

    Cat Finished Ex.

--- src/objectprogramming/TestClass.java
+++ src/objectprogramming/TestClass.java
@@ -6,10 +6,11 @@ public  TestClass(){
 }	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	Worker ken = new Worker();
+	Cat tracker = new Cat();
 	Scanner input = new Scanner(System.in);
-		System.out.println("Your paycheck is " + ken.payCheck() + " dollars a week");
-		ken.changePay(4);
-		System.out.println("Your new hourly pay is " + ken.getNewpay());
+		System.out.println("Cat sleeps "+ tracker.sleepMore() + " hours on a weekday.");
+		System.out.println("Cat sleeps " + tracker.sleepLess() + " hours on a weekend.");
+		System.out.println("Cat is " + tracker.catBirthday() + " years old today");
+		
 }
 }
