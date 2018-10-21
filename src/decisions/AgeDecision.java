6aff8fafbb40413fde910de7291a810e389c5191
Unknown <angelarose.de@gmail.com>
2018-10-21 13:47:33 -0500

    Age Decisions Ex.

---
+++ src/decisions/AgeDecision.java
@@ -0,0 +1,17 @@
+package decisions;
+import java.util.Scanner;
+public class AgeDecision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input = new Scanner(System.in);
+System.out.println("How old are you?");
+int age = input.nextInt();
+System.out.println("I hope you are having a great day!");
+if
+(age <= 17) {
+	System.out.println("You are getting an A in this class."); 
+}
+	}
+
+}
