0fca2bf85043647c39e7b6a3557842c9632bbd14
Unknown <angelarose.de@gmail.com>
2018-10-31 20:38:45 -0500

    Cube Ex.

--- src/decisions/Cube.java
+++ src/decisions/Cube.java
@@ -8,17 +8,16 @@ public class Cube {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int num = -999;
+		System.out.println("Enter an integer");
+		num = input.nextInt();
+		System.out.println(num*num*num);
 		while (num != -1) {
-			if (num==-1) {
-				System.out.println("Error");
-			}
-			else {
 				System.out.println("Enter an integer");
 		num = input.nextInt();
-			System.out.println(num*num*num);
-			}
-		
-			
+			System.out.println(num*num*num);	
+		}
+		if (num==-1) {
+			System.out.println("Error");
 		}
 	}
 	
