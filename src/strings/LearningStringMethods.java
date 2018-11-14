b0b5bf9a2007a440ec50a9c5147026e51db0d1ea
Angela <angelarose.de@gmail.com>
2018-11-14 14:00:20 -0600

    Fix these

---
+++ src/strings/LearningStringMethods.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.Scanner;
+
+public class LearningStringMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		/*
+		 * System.out.println("type a name"); String name1 = input.nextLine();
+		 * String uppername1 = name1.toUpperCase();
+		 * System.out.println(uppername1); System.out.println("type a name");
+		 * String name2 = input.nextLine(); String uppername2 =
+		 * name2.toUpperCase(); if(name1.compareTo(name2) > 0) {
+		 * System.out.println(name2); }
+		 */
+		System.out.println("enter your password");
+		String password = input.nextLine();
+		System.out.println("verify your password");
+		String verificaton = input.nextLine();
+		
+	}
+
+}
