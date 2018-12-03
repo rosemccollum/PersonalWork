49daa3cadd7569ba9a8e1ef47a37f4966bcec3cf
Unknown <angelarose.de@gmail.com>
2018-12-03 16:15:37 -0600

    Friends Ex.

---
+++ src/strings/Friends.java
@@ -0,0 +1,38 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Friends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many friends do you have?");
+		int friends = input.nextInt();
+		String[] names = new String[friends];
+		for (int i = 0; i < friends; i++) {
+			System.out.println("What is your friends name?");
+			names[i] = input.nextLine();
+		}
+		int largest = 0;
+		String large = " ";
+		String small = " ";
+		int smallest = 0;
+		for (int j = 0; j < friends; j++) {
+			if (names[j].length() > largest) {
+				largest = names[j].length();
+				large = names[j];
+			}
+			else {
+				small = names[j];
+				smallest = names[j].length();
+			}
+			System.out.println(names[j]);
+		}
+		System.out.println(large);
+		System.out.println(small);
+		System.out.println(largest-smallest);
+
+	}
+
+}
