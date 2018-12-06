048a4959656eb6cbdce27eca246124a61eb80eec
Unknown <angelarose.de@gmail.com>
2018-12-05 18:16:43 -0600

    Rectangle Array Lists Ex.

---
+++ src/strings/Rectangle.java
@@ -0,0 +1,26 @@
+package strings;
+
+public class Rectangle {
+
+	private int length;
+	private int width;
+
+	public Rectangle(int xLength, int xWidth) {
+		length = xLength;
+		width = xWidth;
+	}
+
+	public int returnLength() {
+		return length;
+	}
+
+	public int returnWidth() {
+		return width;
+	}
+
+	public double returnArea() {
+		double area = length * width;
+		return area;
+	}
+
+}
