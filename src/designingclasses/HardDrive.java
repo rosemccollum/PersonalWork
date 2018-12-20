51f942fa6aa7463c85e691fc088da9b23099e2ec
Angela <angelarose.de@gmail.com>
2018-12-20 13:25:19 -0600

    Hard Drive Ex.

--- src/designingclasses/HardDrive.java
+++ src/designingclasses/HardDrive.java
@@ -1,5 +1,42 @@
 package designingclasses;
 
+import java.util.ArrayList;
+
 public class HardDrive {
 
+	private String name = " ";
+	ArrayList <Double> files = new ArrayList<Double>();
+	private double totalSpace;
+	private double freeSpace;
+	public HardDrive (String xName, double xTotal, double xFree) {
+		name = xName;
+		totalSpace = xTotal;
+		freeSpace = xFree;
+	}
+	public void addFile (double size) {
+		files.add(new Double (size));
+	}
+	public void deleteFile (int index) {
+		files.remove(index);
+	}
+	public boolean isFull() {
+		if (freeSpace == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public boolean isEmpty() {
+		if (freeSpace == totalSpace) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public int returnSize() {
+		return files.size();
+	}
+	
 }
