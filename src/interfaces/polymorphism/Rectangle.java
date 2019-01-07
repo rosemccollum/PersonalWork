51f79ac8b75dcf4c32875b6fab9d9b945715379c
Angela <angelarose.de@gmail.com>
2019-01-07 15:05:13 -0600

    Learning Interfaces/Polymorhpisms

---
+++ src/interfaces/polymorphism/Rectangle.java
@@ -0,0 +1,27 @@
+package interfaces.polymorphism;
+
+public class Rectangle implements Shape {
+	private int length;
+	private int width;
+
+	public Rectangle(int xLength, int xWidth) {
+		length = xLength;
+		width = xWidth;
+	}
+
+	public int returnLength() {
+		return length;
+	}
+
+	public int returnWidth() {
+		return width;
+	}
+
+	public double returnArea() {
+		double area = length * width;
+		return area;
+	}
+	public String toString() {
+		return("Rectangle length: " + length + " width: " + width);
+	}
+}
