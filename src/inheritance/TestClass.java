219ad58265ec6edb3830ed508ebcc7b989d6fc68
Angela <angelarose.de@gmail.com>
2019-01-11 15:08:18 -0600

    Largest Area (figure out object type)

--- src/inheritance/TestClass.java
+++ src/inheritance/TestClass.java
@@ -1,5 +1,5 @@
 package inheritance;
-
+import java.util.ArrayList;
 public class TestClass {
 
 	public static void main(String[] args) {
@@ -16,6 +16,20 @@ public class TestClass {
 		Square thang = new Square(4.23);
 		System.out.println("Square area: " + thang.area());
 		System.out.println("Square perimeter: " + thang.perimeter());
+		ArrayList <Geo> shapes = new ArrayList <Geo>();
+		shapes.add(new Rectangle(1.3,3.4,4.3));
+		shapes.add(new Trapezoid(4.3,5.2,5.3,3.6,1.5));
+		shapes.add(new Rectangle(7.2,1.34,1.53));
+		shapes.add(new Trapezoid(5.2,7.3,9.2,1.4,2.3));
+		shapes.add(new Rectangle(6.2,1.3,5.2));
+		shapes.add(new Trapezoid(5.3,5.6,3.3,3.6,6.4));
+		double largest = 0;
+		for (Geo i : shapes) {
+			if (i.area() > largest) {
+				largest = i.area();
+			}
+		}
+		System.out.println("Largest area: ");
 	}
 
 }
